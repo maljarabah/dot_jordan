2d0649c9fe5621f49ad4d68063f7eda5d08c3d01
maljarabah <maljarabah@users.noreply.github.com>
2025-04-20 20:02:14 +0300

    portfolio project :)

--- project_portfolio/src/components/Contact.jsx
+++ project_portfolio/src/components/Contact.jsx
@@ -13,27 +13,6 @@ function Contact() {
     const [isLoading, setIsLoading] = useState(false)
     const [btnText, setBtnText] = useState("submit")
 
-    const contactMethods = [
-        {
-            icon: (
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
-                </svg>
-            ),
-            contact: "mohjarabah@gmail.com",
-            link: "mailto:mohjarabah@gmail.com",
-        },
-        {
-            icon: (
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-                </svg>
-            ),
-            contact: "+962 797974426",
-            link: "tel:+962797974426",
-        },
-    ]
-
     const sendEmail = (e) => {
         e.preventDefault()
 
@@ -50,8 +29,8 @@ function Contact() {
                     setIsLoading(false)
                     setBtnText("Successfully sent :)")
                 },
-                (error) => {
-                    console.log("EmailJS: FAILED...", error.text)
+                (err) => {
+                    console.log("EmailJS: FAILED...", err.text)
                     setIsLoading(false)
                     setBtnText("Failed to submit :(")
                 }
@@ -61,58 +40,61 @@ function Contact() {
     }
 
     return (
-        <main className="py-14">
-            <div className="mx-auto max-w-screen-xl px-4 text-gray-600 md:px-8">
-                <div className="mx-auto max-w-lg justify-between gap-12 lg:flex lg:max-w-none">
-                    <div className="mb-16 max-w-lg space-y-3">
-                        <p className="text-3xl font-semibold text-gray-800 sm:text-4xl">Get in touch!</p>
-                        <p>Feel free to reach out! Whether you have a question, an opportunity, or just want to say hello, I'd love to hear from you.</p>
+        <div>
+            <div className="max-w-screen-xl mx-auto px-4 text-gray-600 md:px-8">
+                <p className="max-w-lg mx-auto space-y-3 sm:text-center">
+                    Feel free to reach out! Whether you have a question, an opportunity, or just want to say hello, I'd love to hear from you.
+                </p>
+                <div className="mt-12 max-w-lg mx-auto">
+                    <form
+                        ref={form}
+                        onSubmit={sendEmail}
+                        className="space-y-5"
+                    >
                         <div>
-                            <ul className="mt-6 flex flex-wrap items-center gap-x-10 gap-y-6">
-                                {contactMethods.map((item, idx) => (
-                                    <li key={idx} className="flex items-center gap-x-3">
-                                        <div className="flex-none text-gray-400">{item.icon}</div>
-                                        <a href={item.link}>{item.contact}</a>
-                                    </li>
-                                ))}
-                            </ul>
+                            <label htmlFor="user_name" className="font-medium">
+                                Name
+                            </label>
+                            <input
+                                type="text"
+                                name="user_name"
+                                id="user_name"
+                                required
+                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                            />
                         </div>
-                    </div>
-                    <div className="flex-1 sm:max-w-lg lg:max-w-md">
-                        <form ref={form} onSubmit={sendEmail} className="space-y-5">
-                            <div>
-                                <label htmlFor="user_name" className="font-medium">
-                                    Full name
-                                </label>
-                                <input type="text" name="user_name" id="user_name" required className="mt-2 w-full rounded-lg border bg-transparent px-3 py-2 text-gray-500 shadow-sm outline-none focus:border-indigo-600" />
-                            </div>
-                            <div>
-                                <label htmlFor="user_email" className="font-medium">
-                                    Email
-                                </label>
-                                <input type="email" name="user_email" id="user_email" required className="mt-2 w-full rounded-lg border bg-transparent px-3 py-2 text-gray-500 shadow-sm outline-none focus:border-indigo-600" />
-                            </div>
-                            <div>
-                                <label htmlFor="user_message" className="font-medium">
-                                    Message
-                                </label>
-                                <textarea name="user_message" id="user_message" required className="mt-2 h-36 w-full resize-none appearance-none rounded-lg border bg-transparent px-3 py-2 shadow-sm outline-none focus:border-indigo-600"></textarea>
-                            </div>
-                            <button className="flex w-full items-center justify-center rounded-lg bg-indigo-600 px-4 py-2 font-medium text-white duration-150 hover:bg-indigo-500 active:bg-indigo-600">
-                                {isLoading ? (
-                                    <>
-                                        <LoadingSvg />
-                                        <span>Sending...</span>
-                                    </>
-                                ) : (
-                                    btnText
-                                )}
-                            </button>
-                        </form>
-                    </div>
+                        <div>
+                            <label htmlFor="user_email" className="font-medium">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                name="user_email"
+                                id="user_email"
+                                required
+                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="user_message" className="font-medium">
+                                Message
+                            </label>
+                            <textarea name="user_message" id="user_message" required className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"></textarea>
+                        </div>
+                        <button className="flex w-full items-center justify-center rounded-lg bg-indigo-600 px-4 py-2 font-medium text-white duration-150 hover:bg-indigo-500 active:bg-indigo-600">
+                            {isLoading ? (
+                                <>
+                                    <LoadingSvg />
+                                    <span>Sending...</span>
+                                </>
+                            ) : (
+                                btnText
+                            )}
+                        </button>
+                    </form>
                 </div>
             </div>
-        </main>
+        </div>
     )
 }
 
